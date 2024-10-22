24a5e927c2b37b36a6ccfb95db4329a9de696dc5
Daniel Mashid <daniel@ametos.com>
2024-10-22 15:42:57 +0300

    move addToCart to cart.js

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(myProductId) {
+    let matchingItem;
+    
+    cart.forEach((cartItem) => {
+      if (myProductId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    }
+    else {
+      cart.push({
+        productId: myProductId,
+        quantity: 1
+      });
+    }
+  }
