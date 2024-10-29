661f5604c297f24aac4b0a8dce5d9fe67002ed33
Daniel Mashid <daniel@ametos.com>
2024-10-29 18:04:31 +0200

    fix price formatting and radio selectors

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,21 +1,20 @@
-import {cart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
-let cartSummaryHTML = '';
+let cartSummaryHTML = "";
 
 cart.forEach((cartItem) => {
+	const productId = cartItem.productId;
 
-    const productId = cartItem.productId;
+	let matchingProduct;
 
-    let matchingProduct;
-
-    products.forEach((product) => {
-
-        if (product.id === productId) {
-            matchingProduct = product;
-        }
-    })
-cartSummaryHTML += `
+	products.forEach((product) => {
+		if (product.id === productId) {
+			matchingProduct = product;
+		}
+	});
+	cartSummaryHTML += `
     <div class="cart-item-container">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
@@ -30,11 +29,13 @@ cartSummaryHTML += `
                 ${matchingProduct.name}
             </div>
             <div class="product-price">
-                $${matchingProduct.priceCents / 100}
+                $${formatCurrency(matchingProduct.priceCents)}
             </div>
             <div class="product-quantity">
                 <span>
-                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                Quantity: <span class="quantity-label">${
+									cartItem.quantity
+								}</span>
                 </span>
                 <span class="update-quantity-link link-primary">
                 Update
@@ -52,7 +53,7 @@ cartSummaryHTML += `
             <div class="delivery-option">
                 <input type="radio" checked
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Tuesday, June 21
@@ -65,7 +66,7 @@ cartSummaryHTML += `
             <div class="delivery-option">
                 <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Wednesday, June 15
@@ -78,7 +79,7 @@ cartSummaryHTML += `
             <div class="delivery-option">
                 <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Monday, June 13
@@ -92,6 +93,6 @@ cartSummaryHTML += `
         </div>
     </div>
 `;
-})
+});
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
