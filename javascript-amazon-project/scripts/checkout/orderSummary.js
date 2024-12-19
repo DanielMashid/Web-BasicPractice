62d5328f72e19dd8fcdef29eb6c78c5453678f62
Daniel Mashid <daniel@ametos.com>
2024-12-19 15:45:33 +0200

    Save date and generate HTML for payment summary

--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -1,8 +1,11 @@
 import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import formatCurrency from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"; // Importing dayjs from a esm version
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import {
+	deliveryOptions,
+	getDeliveryOption,
+} from "../../data/deliveryOptions.js";
 
 export function renderOrderSummary() {
 	let cartSummaryHTML = "";
@@ -10,23 +13,11 @@ export function renderOrderSummary() {
 	cart.forEach((cartItem) => {
 		const productId = cartItem.productId;
 
-		let matchingProduct;
+		const matchingProduct = getProduct(productId);
 
-		products.forEach((product) => {
-			if (product.id === productId) {
-				matchingProduct = product;
-			}
-		});
+		const deliveryOptionId = cartItem.deliveryOptionId;
 
-		const deliveryOptionsId = cartItem.deliveryOptionsId;
-
-		let deliveryOption;
-
-		deliveryOptions.forEach((option) => {
-			if (option.id === deliveryOptionsId) {
-				deliveryOption = option;
-			}
-		});
+		const deliveryOption = getDeliveryOption(deliveryOptionId);
 
 		const today = dayjs();
 		const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
