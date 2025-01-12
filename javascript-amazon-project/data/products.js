3c0a6fc1919873b32e27ea9fa81f799181c3bf03
Daniel Mashid <daniel@ametos.com>
2025-01-12 17:54:54 +0200

    Load products from the backend

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -54,7 +54,32 @@ class Clothing extends Product {
 	}
 }
 
+export let products = [];
+
+export function loadProducts(fun) {
+	const xhr = new XMLHttpRequest();
+
+	xhr.addEventListener("load", () => {
+		products = JSON.parse(xhr.response).map((productDetails) => {
+			if (productDetails.type === "clothing") {
+				return new Clothing(productDetails);
+			}
+			return new Product(productDetails);
+		});
+
+		console.log("load Products");
+
+		fun(); // callback function (a function that run in the future)
+	});
+
+	xhr.open("GET", "https://supersimplebackend.dev/products");
+	xhr.send();
+}
+
+loadProducts();
+
 // Simulate the product data available in the database
+/*
 export const products = [
 	{
 		id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -532,3 +557,4 @@ export const products = [
 	}
 	return new Product(productDetails);
 });
+*/
